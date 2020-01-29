6667e3389ad24f32cc0a573da83fcce7163eacbb
Yash Desai <yddc902@live.com>
2020-01-29 15:10:31 +0530

    Update bottom.php

--- includes/bottom.php
+++ includes/bottom.php
@@ -1,4 +1,4 @@
-<footer><small>Footer</small></footer>
+<footer><small>Footer</small><p><a  href="https://github.com/YashDDesai/simple-php-template/archive/master.zip">Dowload the sorce code</a></p></footer>
 
 </body>
 </html>
